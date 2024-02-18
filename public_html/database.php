8b474b29ebc127e0529190c4ce90a7eb48b3fa28
mahsandu <mahsandu@gmail.com>
2024-02-18 22:40:13 +0600

    clean start

---
+++ public_html/database.php
@@ -0,0 +1,23 @@
+<?php
+    $host = "localhost";
+    $user = "inventory";
+    $password = "mission@2021";
+    $database = "inventory";
+    $kohadb = "koha_ils";
+    $kohaServer = "localhost";
+    $conn = mysqli_connect($host, $user, $password, $database);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error($conn));
+    }
+ 
+ $koha = mysqli_connect($kohaServer, $user, $password, $kohadb);
+   if (!$koha) {
+       die("Connection failed: " . mysqli_connect_error($koha));
+   } 
+
+    function sanitize($conn, $str){
+        return mysqli_real_escape_string($conn, $str);
+    }
+    // var_dump(function_exists('mysqli_connect'));
+    date_default_timezone_set("Asia/Dhaka");
+?>
